6850eed9d4eb4d974e58c4c937e718cbdf6c0f4d
Mike Boyle <mikesboyle@gmail.com>
2021-05-10 09:14:16 -0400

    move helper methods to filters.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useState, useEffect, useCallback } from 'react';
 import { fetchData } from './helpers/api';
-import { FILTER_NAMES, distinctFieldValues } from './helpers/filters';
+import { filterForNotes, populateFilterOptions } from './helpers/filters';
 
 import Filters from './components/Filters';
 import Map from './components/Map';
@@ -9,22 +9,20 @@ import Map from './components/Map';
 function App() {
   const [loading, setLoading] = useState(true);
   const [sightings, setSightings] = useState([]);
-  const [currentFilters, setCurrentFilters] = useState({});
   const [filterOptions, setFilterOptions] = useState({});
+  const [currentFilters, setCurrentFilters] = useState({});
 
   const fetchSightings = useCallback(async () => {
     setLoading(true);
+
     let data = await fetchData(currentFilters);
-    if (currentFilters['has_notes'] === 'true') {
-      data = data.filter((d) => d.other_activities || d.other_interactions);
-    } else if (currentFilters['has_notes'] === 'false') {
-      data = data.filter((d) => !d.other_activities && !d.other_interactions);
-    }
-    setSightings(data);
-    setLoading(false);
+
     if (Object.keys(filterOptions).length < 1) {
       setFilterOptions(populateFilterOptions(data));
     }
+
+    setSightings(filterForNotes(data, currentFilters['has_notes']));
+    setLoading(false);
   }, [currentFilters, filterOptions]);
 
   useEffect(() => {
@@ -37,16 +35,6 @@ function App() {
       ...currentFilters,
       [name]: value,
     });
-  };
-
-  const populateFilterOptions = (data) => {
-    return FILTER_NAMES.reduce(
-      (filterOptions, name) => {
-        filterOptions[name] = distinctFieldValues(data, name);
-        return filterOptions;
-      },
-      { has_notes: [false, true] }
-    );
   };
 
   const resultText = loading
--- src/helpers/filters.js
+++ src/helpers/filters.js
@@ -1,4 +1,19 @@
-export const distinctFieldValues = (sightings, field) => {
+const FILTER_NAMES = [
+  'date',
+  'primary_fur_color',
+  'highlight_fur_color',
+  'location',
+  'running',
+  'chasing',
+  'climbing',
+  'eating',
+  'foraging',
+  'approaches',
+  'kuks',
+  'quaas',
+];
+
+const distinctFieldValues = (sightings, field) => {
   const values = new Set();
   sightings.forEach((sighting) => {
     let value = sighting[field];
@@ -17,17 +32,23 @@ export const snakeToTitleCase = (string) => {
     .join(' ');
 };
 
-export const FILTER_NAMES = [
-  'date',
-  'primary_fur_color',
-  'highlight_fur_color',
-  'location',
-  'running',
-  'chasing',
-  'climbing',
-  'eating',
-  'foraging',
-  'approaches',
-  'kuks',
-  'quaas',
-];
+export const filterForNotes = (data, filter) => {
+  switch (filter) {
+    case 'true':
+      return data.filter((d) => d.other_activities || d.other_interactions);
+    case 'false':
+      return data.filter((d) => !d.other_activities && !d.other_interactions);
+    default:
+      return data;
+  }
+};
+
+export const populateFilterOptions = (data) => {
+  return FILTER_NAMES.reduce(
+    (filterOptions, name) => {
+      filterOptions[name] = distinctFieldValues(data, name);
+      return filterOptions;
+    },
+    { has_notes: [false, true] }
+  );
+};
