cb7a44f4b420382fc3e55c6aa38af5102c1149b1
Mike Boyle <mikesboyle@gmail.com>
2021-05-10 21:07:31 -0400

    remove commented out code

--- src/components/Map.test.js
+++ src/components/Map.test.js
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { data } from '../helpers/__fixtures__/data';
-// import { FILTER_NAMES } from './helpers/filters';
 import { parseDate } from '../helpers/format';
 
 import Map from './Map';
