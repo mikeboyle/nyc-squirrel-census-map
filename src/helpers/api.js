cb6ebe990460826b91220a63b5c278ac3c91dde2
Mike Boyle <mikesboyle@gmail.com>
2021-05-10 21:51:03 -0400

    api filter test

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,8 +1,10 @@
-const NYC_DATA_TOKEN = process.env['REACT_APP_NYC_DATA_APP_TOKEN'];
+export const NYC_DATA_TOKEN = process.env['REACT_APP_NYC_DATA_APP_TOKEN'];
 export const MAPS_API_KEY = process.env['REACT_APP_MAPS_API_KEY'];
+export const API_URL =
+  'https://data.cityofnewyork.us/resource/vfnx-vebw.json?$limit=5000';
 
 export const fetchData = async (filters = {}) => {
-  let url = 'https://data.cityofnewyork.us/resource/vfnx-vebw.json?$limit=5000';
+  let url = API_URL;
   Object.keys(filters).forEach((key) => {
     if (key !== 'has_notes') {
       const val = filters[key];
